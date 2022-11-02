c2445ba465b671625270c2167ea29e5037680a63
Irene <80887775+ireneburri@users.noreply.github.com>
2022-11-02 16:26:46 +0100

    Pwd criptata

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,7 @@ const ENCRIPTION_KEY = process.env.CRYPT_KEY
 router.get('/', async(req, res)=> {
     try{
         const users = await User.find()
+        users.password = AES.decrypt(users.password, ENCRIPTION_KEY).toString(enc.Utf8)
         res.json(users)
     } catch(err) {
         res.status(500).json({message: err.message})
@@ -20,6 +21,7 @@ router.get('/', async(req, res)=> {
 
 //get one
 router.get('/:id', getUser, (req, res)=> {
+    res.user.password = AES.decrypt(res.user.password, ENCRIPTION_KEY).toString(enc.Utf8)
     res.json(res.user)
 })
 
