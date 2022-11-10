eb26e6dd667a73d20bea3c92cd1378a5c6e9474b
Irene <80887775+ireneburri@users.noreply.github.com>
2022-11-10 20:36:24 +0100

    modifica per api image

---
+++ routes/image.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const router = express.Router()
+
+const path = require('path');
+//require('dotenv').config({ path: path.resolve(__dirname, './.env') })
+
+//per caricare le immagini sul server nella giusta cartella col nome completo:
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null,  path.join(__dirname, './uploads'))        
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+});
+const upload = multer({storage: storage})
+
+
+//UPLOAD DI UN IMMAGINE
+router.post('/', upload.single("file"), async (req, res)=> {
+    console.log(req.file, req.body)
+})
