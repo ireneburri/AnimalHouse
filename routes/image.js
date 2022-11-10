bbce2bf6e1a50d095c4283db67cf398ca41cee12
Irene <80887775+ireneburri@users.noreply.github.com>
2022-11-10 21:24:20 +0100

    modifica per api image

--- routes/image.js
+++ routes/image.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const path = require('path');
 //require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
+const Image = require("./uploads")
+
+
 //per caricare le immagini sul server nella giusta cartella col nome completo:
 const multer = require('multer')
 const storage = multer.diskStorage({
@@ -21,7 +24,39 @@ const upload = multer({storage: storage})
 //UPLOAD DI UN IMMAGINE
 router.post('/', upload.single("file"), async (req, res)=> {
     console.log(req.file, req.body)
+    try{
+        const newimg = await req.file.save()
+        res.status(201).json(newimg)
+    } catch(err) {
+        res.status(400).json({message: err.message})
+    }
 })
+
+
+//DELETE ONE IMAGE
+router.delete('/:name', getImage, async (req, res)=> {
+    try{
+        await res.image.remove()
+        res.json({message: 'Deleted Successfully'})
+    } catch(err){
+        res.status(500).json({message : err.message})
+    }
+})
+
+
+async function getImage (req, res, next){
+    let image
+    try{
+        image = await Image.filename(req.params.name)
+        if(image==null){
+            return res.status(404).json({message : 'Cannot find image'})
+        }
+    } catch(err){
+        return res.status(500).json({message : err.message})
+    }
+    res.image=image
+    next()
+}
 
 
 module.exports = router
