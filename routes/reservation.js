5344869b9435ee3bccdc9134f854507773f06722
Irene <80887775+ireneburri@users.noreply.github.com>
2022-11-25 23:15:42 +0100

    api get all reservations

--- routes/reservation.js
+++ routes/reservation.js
@@ -7,6 +7,7 @@ router.post('/post', async (req, res)=> {
     const reservation = new Reservation({
         username: req.body.username,
         service: req.body.service,
+        time: req.body.time,
         date_start: req.body.date_start,
         date_end: req.body.date_end,
         location: req.body.location,
@@ -82,6 +83,9 @@ router.patch('/:id', getReservation, async (req, res)=> {
     }
     if(req.body.service != null){
         res.reservation.service=req.body.service
+    }
+    if(req.body.time != null){
+        res.reservation.time=req.body.time
     }
     if(req.body.date_start != null){
         res.reservation.date_start=req.body.date_start
