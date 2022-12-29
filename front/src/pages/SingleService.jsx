16502efe72b0bc1fbf6734fb0d11c5513695bda3
Beatrice Zamagna <bebazamagna@gmail.com>
2022-12-29 03:24:52 +0100

    prenotazioni sembrerebbero funzionanti

--- front/src/pages/SingleService.jsx
+++ front/src/pages/SingleService.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import Navbar from '../components/Navbar/navbar';
 import styled from 'styled-components';
 import immagine from '../img/dog.jpg';
-import { useParams } from 'react-router-dom';
-// import Newsletter from '../components/Newsletter/newsletter';
 import Footer from '../components/Footer/footer';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import addDays from "date-fns/addDays";
+import getDayOfYear from "date-fns/esm/getDayOfYear";
 
 
 const Container = styled.div`
@@ -47,11 +55,6 @@ const Category = styled.span`
 const Title = styled.h2`
     font-weight: 200;
 `
-// const Brand = styled.span`
-//     font-weight: 100;
-//     color: grey;
-//     font-size: 15px;
-// `
 const Desc = styled.p`
     margin: 20px 0px;
     font-size: 20px;
@@ -63,81 +66,283 @@ const Price = styled.span`
 const LocationContainer = styled.div`
     margin-top: 1em;
 `
-// const Amount = styled.input`
-//     width: 60px;
-//     height: 30px;
-//     border-radius: 10px;
-//     border: 1px solid teal;
-//     justify-content: center;
-// `
-// const AmountContainer = styled.div`
-
-// `
-// const Button = styled.button`
-//     margin-left: 2em;
-//     border: 1px solid teal;
-//     background-color: white;
-//     color: teal;
-//     cursor: pointer;
-//     font-weight: 900;
-//     border-radius: 10px;
-//     padding-left: 15px;
-//     padding-right: 15px;
-//     text-align: center;
-//     &:hover{
-//         background-color: teal;
-//         color: white;
-//         font-weight: 900;
-//         transition: all 0.4s ease;
-//     }
-// `
 
 function SingleService() {
-
+    const navigate = useNavigate();
     const data = useParams();
-    const id = data.id;
-    // const {items} = useFetch(`https://site212224.tw.cs.unibo.it/Service/id/${id.substring(1)}`);
+    const name = data.name;
 
     const [datas, setDatas] = useState([]);
+    const duration = datas.time
+
     const [locations, setLocations] = useState([]);
-    // setLocations(items.location)
-    // console.log(locations)
+    const [loc, setLoc] = useState("")
+
+    //data attuale + 1 e orario settato alle 8:00
+    const [dateTime, setDateTime] = useState(setHours(setMinutes(new Date().setDate(new Date().getDate()), 0), 8));
+    const [endDate, setEndDate] = useState(null);
+
+    const [booking, setBooking] = useState([])
+    const [bookingDays, setBookingDays] = useState([])
+    const [bookingDaysForCheckFormat, setBookingDaysForCheckFormat] = useState([])
+
+    var booked = []
 
     useEffect(() => {
+        console.log(loc)
         fetchData();
+        fetchBookings();
         // eslint-disable-next-line
-    }, []);
+    }, [loc, dateTime]);
+
+    const onChange = (dates) => {
+        const [start, end] = dates;
+        setDateTime(start);
+        setEndDate(end);
+        //console.log(start);
+        console.log(end);
+    }; //setta data di inizio e data di fine
 
     const fetchData = async () => {
-        const items = await fetch(`https://site212224.tw.cs.unibo.it/Service/id/${id.substring(1)}`);
+        const items = await fetch(`https://site212224.tw.cs.unibo.it/Service/name/${name.substring(1)}`);
         const fetched = await items.json();
 
-        setDatas(fetched)
-        setLocations(fetched.location);
+        setDatas(fetched[0])
+        setLocations(fetched[0].location);
+    }
+
+    const changeFormat = (e) => {
+        e.start = setMinutes(new Date(e.start), 0, 0);
+        e.end = setMinutes(new Date(e.end), 0, 0);
+        return e;
+    } //setta a 0 i minuti
+
+    const fetchBookings = async () => {
+        const data = await fetch("https://site212224.tw.cs.unibo.it/Reservation");
+        const items = await data.json();
+
+        for (let booking in items) {
+            if (items[booking].service === name.substring(1) && items[booking].location === loc)
+                booked.push({ start: items[booking].date_start, end: items[booking].date_end })
+        }
+
+        booked = booked.map(e => (
+            e = changeFormat(e)
+        ))
+
+        console.log(booked)
+
+        var arrayBooking = [];
+        var arrayBookingDays = [];
+        setBookingDaysForCheckFormat(booked);
+
+        for (let i in booked) {
+            if (booked[i].start.toDateString() === dateTime.toDateString()) {
+                // se la data di inizio è = alla data che selezioni 
+                let x = booked[i].start.getHours();
+                let z = booked[i].end.getHours();
+                while (x < z) {
+                    arrayBooking.push(setHours(setMinutes(new Date(dateTime), 0), x++));
+                    //blocco tutte le ore singolarmente di un singolo appuntamento
+                }
+            }
+        }
+
+        for (let i in booked) {
+            let x = getDayOfYear(booked[i].start);
+            let z = getDayOfYear(booked[i].end);
+            let j = 0;
+            while (x <= z) {
+                arrayBookingDays.push(addDays(booked[i].start, j++));
+                x++;
+                //blocco tutte le date di un appuntamento a giorni
+            }
+        }
+
+        setBooking(arrayBooking);
+        console.log(arrayBooking)
+        setBookingDays(arrayBookingDays);
+        // console.log(arrayBookingDays)
+    }
+
+    function addHoursToDate(date, hours) {
+        return new Date(new Date(date).setHours(new Date(date).getHours() + (hours)));
+    } //agagiunge x ore alla x data
+
+    function dateOverlap(start, end, bookingList) {
+        start = new Date(start).getTime()
+        end = new Date(end).getTime()
+        console.log(bookingList);
+        for (let b in bookingList) {
+            let bStart = new Date(bookingList[b]['start']).getTime()
+            let bEnd = new Date(bookingList[b]['end']).getTime()
+
+            if (start >= bStart && start <= bEnd) { //a tra c e d
+                return true
+            }
+            if (start >= bStart && start <= bEnd) {//b tra c e d
+                return true
+            }
+            if (bStart >= start && bStart <= end) {//c tra a e b
+                return true
+            }
+            if (bEnd >= start && bEnd <= end) {//d tra a e b
+                return true
+            }
+        }
+        return false
+    }
+
+    function dateDiffInDays(a, b) { //differenza tra due date
+        const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+        // Discard the time and time-zone information.
+        const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+        const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+        return (Math.ceil((utc2 - utc1) / _MS_PER_DAY) + 1);
+    }
+
+    const handleSubmit = () => {
+        // console.log(datas.allday)
+        // console.log(Number(datas.time))
+        let start = setHours(new Date(dateTime), dateTime.getHours())
+        console.log(start)
+        var end; var total;
+        if (!datas.allday) { //stesso giorno
+            var tmp = addHoursToDate(start, Number(datas.time)) //data di fine
+            end = setHours(new Date(tmp), tmp.getHours())
+            total = datas.price;
+        } else {
+            end = setHours(endDate, 19);
+            console.log(dateDiffInDays(start, end));
+            total = datas.price * dateDiffInDays(start, end); //calcoli il prezzo per diffenza in giorni
+            console.log(total)
+        }
+
+        const body = {
+            username: localStorage.username,
+            service: datas.name,
+            time: datas.time,
+            date_start: start,
+            date_end: end,
+            location: loc,
+            total: Math.ceil(total), //arrotondare per eccesso
+            allday: datas.allday
+        }
+
+        let containsBookedTimes = false;
+        let containsBookedDays = false;
+
+        if (!datas.allday) {
+            for (let x = 0; x < datas.time; x++) {
+                for (let h in booking) {
+                    console.log(booking[h].getHours())
+                    if (booking[h].getHours() === (start.getHours() + x)) {
+                        containsBookedTimes = true; //prenotazioni in ore
+                    }
+                }
+            }
+        }
+        else {
+            console.log(bookingDaysForCheckFormat);
+            containsBookedDays = dateOverlap(start, end, bookingDaysForCheckFormat);
+            // prenotazioni in giorni
+        }
+
+        if (end === undefined) {
+            console.log("alert 2")
+        }
+        else if (containsBookedTimes) {
+            console.log("alert 3")
+        }
+        else if (containsBookedDays) {
+            console.log("alert 4")
+        }
+        else {
+            console.log(body); //richiesta api
+            axios.post("http://site212224.tw.cs.unibo.it/Reservation/post",
+                body).then(res =>
+                    console.log(res)
+                ).then(() => navigate('/account'))
+        }
     }
 
     return (
         <Container>
             <Navbar />
             <Wrapper className='container'>
-                <ImgContainer>
+                <ImgContainer className='col-lg-4'>
                     <Image src={immagine} />
                 </ImgContainer>
-                <InfoContainer>
+                <InfoContainer className='col-lg-5'>
                     <Category> {datas.category} </Category>
                     <Title style={{ marginTop: '1em', marginBottom: '0px' }}> {datas.name} </Title>
                     <Desc> Description: {datas.description} </Desc>
+                    <p> Duration: {datas.allday ? "all day" : datas.time} </p>
                     <Price> Price: €{datas.price} </Price>
 
-                    <LocationContainer>
-                        <select className="form-select" aria-label="Default select example">
+                    <LocationContainer > 
+                        {datas.mode === "At Home" || datas.mode === "online" ? null :
+                        <select id="locSelector" className="form-select" aria-label="Default select example" onChange={() => setLoc(document.getElementById("locSelector").value)}>
+                            <option value="select location" key="default" disabled > select location </option>
                             {locations.map(loc => (
                                 <option value={loc} key={loc}>{loc}</option>
                             ))};
-                        </select>
+                        </select>}
                     </LocationContainer>
                 </InfoContainer>
-                <hr/>
+
+                <div style={{marginLeft: '2em'}}>
+                    {!datas.allday && <form>
+                        <div className="section">
+                            <h4 className="section-title mb-2">Seleziona data e orario</h4>
+                            <div className="section-content">
+                                <DatePicker
+                                    selected={dateTime}
+                                    onChange={date => setDateTime(date)}
+                                    showTimeSelect
+                                    timeFormat="HH:mm"
+                                    timeIntervals={60}
+                                    timeCaption="time"
+                                    dateFormat="d MMMM yyyy - h:mm"
+                                    minTime={setHours(setMinutes(new Date(), 0), 8)}
+                                    maxTime={setHours(setMinutes(new Date(), 0), (19 - datas.time))}
+
+                                    excludeTimes={
+                                        booking
+                                    }
+                                    minDate={new Date().setDate(new Date().getDate() + 1)}
+                                />
+                            </div>
+                        </div>
+                        <hr></hr>
+
+                        <div className="section">
+                        </div>
+                        <button className="btn btn-warning" onClick={handleSubmit}> Prenota </button>
+                    </form>}
+                    {datas.allday && <form>
+
+                        <div className="section">
+                            <h4 className="section-title mb-2">Seleziona data di inizio e di fine</h4>
+                            <div className="section-content">
+                                <DatePicker
+                                    selected={dateTime}
+                                    onChange={onChange}
+                                    startDate={dateTime}
+                                    endDate={endDate}
+                                    excludeDates={bookingDays}
+                                    selectsRange
+                                    minDate={new Date().setDate(new Date().getDate() + 1)}
+                                    inline
+                                />
+                            </div>
+                        </div>
+                        <hr></hr>
+                        <button className="btn btn-warning" onClick={handleSubmit}> Prenota </button>
+                    </form>}
+                </div>
+
             </Wrapper>
             <Footer />
 
